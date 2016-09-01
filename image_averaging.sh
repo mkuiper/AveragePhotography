64d12d36a3c94a1bb33c003a96ede3d742fede17
mkuiper <kuiper.mike@gmail.com>
2016-09-02 07:41:19 +1000

    getting crontab to work.

--- image_averaging.sh
+++ image_averaging.sh
@@ -1,34 +1,42 @@
-#!/bin/bash 
+#!/bin/bash
+# A script to do the average processing of images in workdirectory.
 
-#  a script to do the average processing of images in workdirectory.
+cd /home/pi/Desktop/AveragePhotography/
 
-LOCATION="TestKitchen"
+if [ -f "Timelapse_Config.txt" ]; then
+ source "Timelapse_Config.txt"
+else 
+ echo " Can't see configuration file: Timelapse_Config.txt. Exiting." 
+ exit 
+fi
 
-TOPDIR=/$HOME/Desktop/AveragePhotography/
-WRKDIR=/$TOPDIR/WorkingDirectory/
-ARCHIVE=/$TOPDIR/FrameArchive/
+#>---------------------------------------------------------------------
+cd $WRKDIR 
 
-START=`date +%Y-%m-%d_%H:%M`
-echo $(printf "Start of image averaging: $s"  $START \n) > logfile.txt 
+DATE=`date +%Y-%m-%d`
+TIME=`date +%H:%M`
+time1=`date +%s`
+FILECOUNT=$(find * -type f | wc -l)
 
+echo $(printf "Start of image averaging: $s  $s"  "$DATE" "$TIME") >>logfile.txt
+echo "-making daily average image"
+imgname=$(printf "%s_%s.jpg" "$LOCATION" "$DATE" )
+convert *.jpg -average $imgname
 
- cd $WRKDIR
- echo "-making daily average image"
- imgname=$(printf "%s_%s.tif" "$LOCATION" "$OLDDATE" )
- convert *.tif -average $imgname
- 
- convert $imgname $TOPDIR/Ref_image.jpg
- mv $imgname $ARCHIVE  
- rm *.tif *.jpg
+cp $imgname $TOPDIR/Ref_image.jpg
+mv $imgname $ARCHIVE
+rm *.jpg *.tif
 
- echo $OLDDATE
- DATE=`date +%Y%m%d`
- OLDDATE=$DATE
+echo $OLDDATE
+DATE=`date +%Y%m%d`
+OLDDATE=$DATEx
 
- cd $TOPDIR
- echo $DATE, $OLDDATE, $d "done"
+cd $TOPDIR
+echo $DATE, $OLDDATE, $d "done"
 
- FINISH=`date +%Y-%m-%d_%H:%M`
- echo $(printf "Finish of image averaging: $s \n"  $FINISH) >> logfile.txt 
-
+FINISH=`date +%Y-%m-%d_%H:%M`
+time2=`date +%s`
+let time3=" $time2 - $time1 "
+echo $(printf "Finish of image averaging: $s \n"  "$FINISH") >> logfile.txt 
+echo $(printf "Processing time: %s seconds for %s images \n"  "$time3" "$FILECOUNT") >> logfile.txt
 
--- start_timelapse.sh
+++ start_timelapse.sh
@@ -4,17 +4,18 @@
 # Note: this script stops at midnight. Ensure to commit a proper schedule
 # in the crontab. Make sure to read the Job_scheduling.README
 
-# Define image capture interval in seconds.
-INTERVAL=300
-# A short label for location.
-LOCATION="Test_Kitchen"
+# You should only have to edit the Timelapse_Config.txt file. 
 
-TOPDIR=/$HOME/Desktop/AveragePhotography/
-WRKDIR=/$TOPDIR/WorkingDirectory/
-ARCHIVE=/$TOPDIR/FrameArchive/
+cd /home/pi/Desktop/AveragePhotography/
 
 
-#>--You shouldn't need to change anything below this line!------------
+# Read in configuration variables.  
+if [ -f "Timelapse_Config.txt" ]; then
+ source "Timelapse_Config.txt"
+else 
+ echo " Can't see configuration file: Timelapse_Config.txt. Exiting." 
+ exit 
+fi
 #>--------------------------------------------------------------------
 cd $TOPDIR
 
@@ -30,11 +31,11 @@ STARTDATE=`date +%Y-%m-%d`
 DATE=$STARTDATE
 TIME=`date +%H:%M`
 
-echo $(printf "Starting dailytime lapse on %s at %s \n" "$DATE" "TIME" ) >>logfile.txt 
-echo $(printf "Location: %s \t Time interval between shots: %s \n" "$LOCATION" "$INTERVAL" ) >>logfile.txt
+echo $(printf "Starting timelapse on %s at %s " "$DATE" "$TIME" ) >>logfile.txt 
+echo $(printf "Location: %s    Time interval between shots: %s seconds " "$LOCATION" "$INTERVAL" ) >>logfile.txt
 
 # Take initial reference image
-echo " taking intial reference image for alignments. "
+echo "-taking intial reference image for alignments. "
 raspistill -p 10,10,640,480 -ev -4 -o  Ref_image.jpg
 
 # Checks for new day; (timelapse needs to be restarted daily in crontab).
@@ -48,7 +49,7 @@ while [ "$DATE" == "$STARTDATE" ]
 # Take photos for HDR:
   echo "-grabbing images for HDR"
   raspistill -p 10,10,320,240 -ISO 100 -ev  0  -q 100  -o temp_image1.jpg
-  raspistill -p 10,10,320,240 -ISO 100 -ev  16 -q 100  -o temp_image2.jpg
+  raspistill -p 10,10,320,240 -ISO 100 -ev  24 -q 100  -o temp_image2.jpg
   raspistill -p 10,10,320,240 -ISO 100 -ev -24 -q 100  -o temp_image3.jpg
 
 # Align image batch (uses reference image as first image):
@@ -63,13 +64,15 @@ while [ "$DATE" == "$STARTDATE" ]
 
 # Label HDR image with timestamp, move to Working directory & cleanup old files.
   TIMENOW=`date +%Y-%m-%d_%H.%M`
-  filename=$(printf "%s/%s_%s_%04d.tif" "$WRKDIR" "$LOCATION" "$TIMENOW" "$COUNTER" )
-  mv aligned.tif $filename
-  rm temp_image* ALIGN_*
+  filename=$(printf "%s/%s_%s_%04d.jpg" "$WRKDIR" "$LOCATION" "$TIMENOW" "$COUNTER" )
+# convert to jpg and cleanup:
+  convert aligned.tif -quality 100 $filename
+  rm temp_image* ALIGN_* aligned.tif
 
 # A conditional test to stop the timelapse (checks for presence of flag file).
   if [ -f "pause_timelapse.txt" ]; then
-   echo "pause flag present > exiting "
+   echo "-pause flag present > exiting "
+   echo $(printf "Pause flag present: stopping time lapse at %s" "$TIMENOW" ) >>logfile.txt
    exit
   fi
 
@@ -80,11 +83,11 @@ while [ "$DATE" == "$STARTDATE" ]
  if [ $INTERVAL -ge $time3 ]
   then
    let delay=" $INTERVAL - $time3 "
-   echo $(printf "Processing time: %s Sleeping for %s seconds" "$time3" "$delay"
+   echo $(printf "Processing time: %s Sleeping for %s seconds" "$time3" "$delay")
    sleep $delay
    else
-    echo "Warning: processing time takes longer than defined timelapse interval. "
-    echo  $(printf "Interval time: %s   processing time: %s" "$INTERVAL" "$time3")
+   echo "Warning: HDR Image processing takes longer than defined timelapse interval."
+   echo $(printf "Interval time: %s   processing time: %s" "$INTERVAL" "$time3")
   fi
 
  done
